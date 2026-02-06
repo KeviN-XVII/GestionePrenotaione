418ffd39cdbe94bfdc1c23473eb64d97f7b8e966
KeviN-XVII <kevinquartar@gmail.com>
2026-02-06 10:05:11 +0100

    Initial commit

---
+++ src/main/java/kevinquarta/GestionePrenotaione/GestionePrenotaioneApplication.java
@@ -0,0 +1,13 @@
+package kevinquarta.GestionePrenotaione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePrenotaioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePrenotaioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kevinquarta/GestionePrenotaione/GestionePrenotaioneApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinquarta.GestionePrenotaione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotaioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
